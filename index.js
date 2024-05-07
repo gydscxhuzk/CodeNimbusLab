b59b0980e0b0d945fd1140f25134e10a3973fb22
gydscxhuzk <gs1paKilla7@hotmail.com>
2024-05-07 20:12:41 +0700

    start project commit

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-function hasCycle(head) {
-  let slow = head;
-  let fast = head;
-  while (fast && fast.next) {
-    slow = slow.next;
-    fast = fast.next.next;
-    if (slow === fast) return true;
-  }
-  return false;
+function greet(name) {
+  console.log("Hello, " + name + "!");
 }
+
+greet("Bob");
