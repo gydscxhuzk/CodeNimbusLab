4b6c2bf2ac48be21dad0919105ee2d7439c4769b
gydscxhuzk <gs1paKilla7@hotmail.com>
2024-05-08 11:50:35 +0700

    test

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-function greet(name) {
-  console.log("Hello, " + name + "!");
+function rob(nums) {
+  if (nums.length === 1) return nums[0];
+  const robRange = (start, end) => {
+    let prevMax = 0;
+    let currMax = 0;
+    for (let i = start; i <= end; i++) {
+      const temp = currMax;
+      currMax = Math.max(currMax, prevMax + nums[i]);
+      prevMax = temp;
+    }
+    return currMax;
+  };
+  return Math.max(robRange(0, nums.length - 2), robRange(1, nums.length - 1));
 }
-
-greet("Bob");
